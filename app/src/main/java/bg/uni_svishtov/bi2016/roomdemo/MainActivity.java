feda4923a6ef602ec1bd6411b88e29b9c62bb839
Angelin Lalev <lalev@uni-svishtov.bg>
2020-01-22 17:21:38 +0200

    Implemented database functionality

--- app/src/main/java/bg/uni_svishtov/bi2016/roomdemo/MainActivity.java
+++ app/src/main/java/bg/uni_svishtov/bi2016/roomdemo/MainActivity.java
@@ -2,9 +2,13 @@ package bg.uni_svishtov.bi2016.roomdemo;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
+import android.app.Application;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,10 +16,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     public static final int START_PRODUCT=1;
 
     private LinearLayout listLayout;
+    private int elementCounter;
+    private AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listLayout = findViewById(R.id.linearLayout);
+        elementCounter=0;
+
+        db = Room.databaseBuilder(
+                getApplication(),
+                AppDatabase.class,
+                "bg.uni_svishtov.bi2016.database")
+                .allowMainThreadQueries()
+                .build();
+
+        loadAllProducts();
+
+    }
+
+    private void loadAllProducts() {
+        List<Product> productsList = db.productDao().getAllProducts();
+        for (Product p : productsList) {
+            addProduct(p.getProductCode(), p.getProductName(), p.getProductDescription());
+        }
+    }
+
+    private void storeProduct(int productCode, String productName, String productDescription) {
+        Product p = new Product(productCode, productName, productDescription);
+        db.productDao().insert(p);
     }
 
     public void onClick(View view) {
@@ -41,6 +72,7 @@ public class MainActivity extends AppCompatActivity {
                         int productCode = data.getIntExtra("productCode", 0);
                         String productName = data.getStringExtra("productName");
                         String productDescription = data.getStringExtra("productDescription");
+                        storeProduct(productCode, productName, productDescription);
                         addProduct(productCode, productName, productDescription);
                     } catch (NullPointerException e) {
                         //Do nothing and return
@@ -55,9 +87,13 @@ public class MainActivity extends AppCompatActivity {
         try {
             LinearLayout layout = (LinearLayout) layoutInflater.inflate(R.layout.list_item,
                     listLayout, false);
-            ((TextView)layout.findViewById(R.id.product_code_view)).setText(productCode);
+            ((TextView)layout.findViewById(R.id.product_code_view)).setText(""+productCode);
             ((TextView)layout.findViewById(R.id.product_name_view)).setText(productName);
-            ((TextView)layout.findViewById(R.id.product_code_view)).setText(productDescription);
+            ((TextView)layout.findViewById(R.id.product_description_view)).setText(productDescription);
+
+            if (elementCounter++%2==1) {
+                layout.setBackgroundColor(Color.rgb(220, 220, 255));
+            }
             listLayout.addView(layout);
 
         } catch (NullPointerException e) {
--- app/src/main/java/bg/uni_svishtov/bi2016/roomdemo/ProductActivity.java
+++ app/src/main/java/bg/uni_svishtov/bi2016/roomdemo/ProductActivity.java
@@ -2,13 +2,38 @@ package bg.uni_svishtov.bi2016.roomdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ProductActivity extends AppCompatActivity {
+
+    private EditText productCodeEdit;
+    private EditText productNameEdit;
+    private EditText productDescriptionEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+
+        productCodeEdit = findViewById(R.id.product_code_edit);
+        productNameEdit = findViewById(R.id.product_name_edit);
+        productDescriptionEdit = findViewById(R.id.product_description_edit);
+    }
+
+    public void onClick1(View view) {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    public void onClick2(View view) {
+        Intent data = new Intent();
+        data.putExtra("productCode", Integer.valueOf(productCodeEdit.getText().toString()));
+        data.putExtra("productName", productNameEdit.getText().toString());
+        data.putExtra("productDescription", productDescriptionEdit.getText().toString());
+        setResult(RESULT_OK, data);
+        finish();
     }
 }
