c33366b3039c10ddbcfbf9d09c3e3cc530c6e421
Angelin Lalev <lalev@uni-svishtov.bg>
2020-01-22 14:05:37 +0200

    Initial commit

---
+++ app/src/main/java/bg/uni_svishtov/bi2016/roomdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package bg.uni_svishtov.bi2016.roomdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
